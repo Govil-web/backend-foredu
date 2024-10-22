df5ba4cd71316f907023533d6e0c6cd3ecac2de5
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-10-21 19:14:42 -0500

    archivos de configuracion ej: target y .idea

--- ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/asistencia/AsistenciaMapperImpl.java
+++ ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/asistencia/AsistenciaMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-07T17:37:14-0500",
+    date = "2024-10-08T21:48:50-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
