52e21495a381157831d2d3e72d0de80afacd2eb4
Jose Angel Suarez <angelsuarez2612@gmail.com>
2024-11-16 13:08:24 -0300

    actualizando deploy con Asistencia funcional y correcion de errores

--- ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/grado/GradoMapperImpl.java
+++ ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/grado/GradoMapperImpl.java
@@ -8,10 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-
-    date = "2024-11-16T11:59:16-0300",
+    date = "2024-11-16T13:06:42-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22 (Oracle Corporation)"
-
 )
 @Component
 public class GradoMapperImpl extends GradoMapper {
