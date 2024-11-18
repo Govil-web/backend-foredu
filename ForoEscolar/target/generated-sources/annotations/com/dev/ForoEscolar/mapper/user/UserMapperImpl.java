bccd75c8f3cfbdd804151d65ac42f94dc37bd8f7
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-11-17 21:25:09 -0500

    feat: se agrega domimio foredu.globalia-tech.com al CORS

--- ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/user/UserMapperImpl.java
+++ ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/user/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-17T21:22:12-0500",
+    date = "2024-11-17T21:24:48-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.1 (Oracle Corporation)"
 )
 @Component
