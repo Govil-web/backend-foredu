8e28566a1f5903b5739b8e2c861ae6c8ee81952d
Jose Angel Suarez <angelsuarez2612@gmail.com>
2025-05-12 22:09:45 -0300

    Fix: Se agrega CloudinaryConfig

---
+++ src/main/java/com/foroescolar/utils/cloudinary/CloudinaryService.java
@@ -0,0 +1,18 @@
+package com.foroescolar.utils.cloudinary;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CloudinaryService {
+
+//    private final Cloudinary cloudinary;
+//
+//    public CloudinaryService(Cloudinary cloudinary) {
+//        this.cloudinary = cloudinary;
+//    }
+//
+//    public String uploadImage(MultipartFile file, String url) {
+//        return cloudinary.uploader().upload(file).get("secure_url").toString();
+//    }
+
+}
